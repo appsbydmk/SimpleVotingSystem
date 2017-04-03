f35657109042c1ac5702fe2ebc104d196ddd7e34
Darshan Kamble <darshankamble1992@gmail.com>
2017-04-03 17:13:39 +0530

    Added MainActivity and created SplashActivity. Created a theme for SplashActivity. Set the theme of SplashActivity to "SplashTheme."

--- app/src/main/java/com/appsbydmk/simplevotingsystem/SplashActivity.java
+++ app/src/main/java/com/appsbydmk/simplevotingsystem/SplashActivity.java
@@ -1,12 +1,19 @@
 package com.appsbydmk.simplevotingsystem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.appsbydmk.simplevotingsystem.activities.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
+    Intent mainIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+        this.startActivity(mainIntent);
+        finish();
     }
 }
