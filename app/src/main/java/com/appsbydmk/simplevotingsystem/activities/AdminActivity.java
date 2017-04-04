22dde4c5d17f4e547e74b3cff96b472ade46c1ff
Darshan Kamble <darshankamble1992@gmail.com>
2017-04-04 18:34:00 +0530

    Created Admin Login Screen. Made UI changes in Login activity of voter.

---
+++ app/src/main/java/com/appsbydmk/simplevotingsystem/activities/AdminActivity.java
@@ -0,0 +1,17 @@
+package com.appsbydmk.simplevotingsystem.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.appsbydmk.simplevotingsystem.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
--- app/src/main/java/com/appsbydmk/simplevotingsystem/activities/MainActivity.java
+++ app/src/main/java/com/appsbydmk/simplevotingsystem/activities/MainActivity.java
@@ -1,15 +1,30 @@
 package com.appsbydmk.simplevotingsystem.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.appsbydmk.simplevotingsystem.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvAdmin;
+    Intent adminIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvAdmin = (TextView) this.findViewById(R.id.tv_admin);
+        tvAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adminIntent = new Intent(MainActivity.this, AdminActivity.class);
+                MainActivity.this.startActivity(adminIntent);
+            }
+        });
     }
 }
